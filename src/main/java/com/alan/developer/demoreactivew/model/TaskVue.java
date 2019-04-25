b0c41f9c027861f47d13ff72a2d97cb08fa165f7
Alan <ustargab@gmail.com>
2019-04-25 07:09:02 +0200

    new asyn queue to user

---
+++ src/main/java/com/alan/developer/demoreactivew/model/TaskVue.java
@@ -0,0 +1,16 @@
+package com.alan.developer.demoreactivew.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class TaskVue {
+    private String uuid;
+    private String title;
+    private String description;
+    private Long start;
+    private Long end;
+    private Long duration;
+    private Integer progress;
+}
--- src/main/java/com/alan/developer/demoreactivew/web/WebsocketController.java
+++ src/main/java/com/alan/developer/demoreactivew/web/WebsocketController.java
@@ -1,12 +1,19 @@
 package com.alan.developer.demoreactivew.web;
 
 import com.alan.developer.demoreactivew.model.EventVue;
+import com.alan.developer.demoreactivew.model.TaskVue;
 import lombok.extern.java.Log;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.UUID;
 
 @Log
 @Controller
@@ -26,5 +33,23 @@ public class WebsocketController {
         log.info("New message");
         headerAccessor.getSessionAttributes().put("username", "Alan");
         return chatMessage;
+    }
+
+
+    @MessageMapping("/process/new")
+    @SendToUser("/queue/tasks")
+    public TaskVue newTask(@Payload TaskVue task, SimpMessageHeaderAccessor headers) {
+        log.info("Session ID->" + headers.getSessionId());
+        final LocalDateTime current = LocalDateTime.now();
+        final Long duration = RandomUtils.nextLong(2, 15);
+        return TaskVue.builder()
+                .uuid(UUID.randomUUID().toString())
+                .title(task.getTitle())
+                .description(task.getDescription())
+                .start(current.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli())
+                .end(current.plusMinutes(duration).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli())
+                .duration(duration)
+                .progress(1)
+                .build();
     }
 }
